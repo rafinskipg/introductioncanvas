d3453097a6985bf5f68af669dd24cf92dace7c8f
Rafael Pedrola <rafinskipg@gmail.com>
2015-01-26 21:33:17 +0100

    change hexagon

--- solutions/chapter_1/hexagon/app.js
+++ solutions/chapter_1/hexagon/app.js
@@ -12,17 +12,26 @@ function drawPolygon(x, y, radius, sides, startAngle, anticlockwise) {
   context.translate(x,y);
   context.rotate(startAngle);
   context.moveTo(radius,0);
+
   for (var i = 1; i < sides; i++) {
     context.lineTo(radius*Math.cos(a*i),radius*Math.sin(a*i));
   }
+
   context.closePath();
 
+  //Draw the shadow (try changing the order of drawing the shadow to be the last operation )
   context.shadowColor = 'rgba(0,0,0,0.75)';
   context.shadowOffsetX = 8;
   context.shadowOffsetY = 8;
   context.shadowBlur = 10;
   context.fill();
 
+  //Draw the border
+  context.strokeStyle = 'rgb(56, 29, 181)';
+  context.lineWidth = 5;
+  context.stroke();
+
+  //Fill it with a gradient
   var gradient = context.createLinearGradient(-x/2, -y/2, radius, radius );
   gradient.addColorStop(0, "rgba(56, 29, 181, 0.5)");
   gradient.addColorStop(0.45, "rgba(96, 72, 208, 0.95)");
@@ -31,17 +40,13 @@ function drawPolygon(x, y, radius, sides, startAngle, anticlockwise) {
   gradient.addColorStop(1, "rgba(56, 29, 181, 0.5)");
   context.fillStyle = gradient;
   context.fill();
+
   context.restore();
 
 }
 
 function render(){
-  // Fill with gradient
-  context.strokeStyle = 'rgb(56, 29, 181)';
-  context.lineWidth = 5;
-
   drawPolygon(200, 200, 100, 6, 0);
-  context.stroke();
 }
 
 render();
